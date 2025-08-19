536476f166c928c163caf61323539607c42ab0e6
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-08-19 13:12:41 -0300

    GlobaExceptionHandler
    - Criado tratamento global de exceptions.

---
+++ src/main/java/br/com/api/coffebank/config/GlobalExceptionHandler.java
@@ -0,0 +1,78 @@
+package br.com.api.coffebank.config;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import br.com.api.coffebank.dto.resposta.ErroCampoDto;
+import br.com.api.coffebank.dto.resposta.ErroRespostaDto;
+import br.com.api.coffebank.exception.CpfJaExisteException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolationException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(RuntimeException.class)
+	public ResponseEntity<ErroRespostaDto> handlerGenerico(RuntimeException ex, HttpServletRequest request){
+		ErroRespostaDto erro = new ErroRespostaDto(
+				ex.getMessage(), 
+				HttpStatus.INTERNAL_SERVER_ERROR.value(), 
+				request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(erro);
+	}
+	
+	@ExceptionHandler(CpfJaExisteException.class)
+	public ResponseEntity<ErroRespostaDto> handlerValidaSeCpfJaExiste(CpfJaExisteException ex, HttpServletRequest request){
+		ErroRespostaDto erro = new ErroRespostaDto(
+				ex.getMessage(),
+				HttpStatus.CONFLICT.value(),
+				request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.CONFLICT).body(erro);
+	}
+	
+	//Esse handler é disparado quando a falha de validação em objetos do corpo da requisição.
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErroRespostaDto> handlerMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpServletRequest request){
+		
+		List<ErroCampoDto> erros = ex.getBindingResult()
+				.getFieldErrors()
+				.stream()
+				.map(erro -> new ErroCampoDto(erro.getField(), erro.getDefaultMessage()))
+				.toList();
+		
+		ErroRespostaDto resposta = new ErroRespostaDto(
+				"Campos inválidos",
+				HttpStatus.BAD_REQUEST.value(), 
+				request.getRequestURI(),
+				erros);
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resposta);
+	}
+	
+	//Esse handler é disparado quando a falha de validação no parametro de entrada.
+	@ExceptionHandler(ConstraintViolationException.class)
+	public ResponseEntity<ErroRespostaDto> handerconstraintViolation(ConstraintViolationException ex, HttpServletRequest request){
+		
+		List<ErroCampoDto> erros = ex.getConstraintViolations()
+				.stream()
+				.map(violation -> new ErroCampoDto(
+						violation.getPropertyPath().toString(), 
+						violation.getMessage()))
+				.toList();
+		
+		ErroRespostaDto resposta = new ErroRespostaDto(
+				"Campos inválidos",
+				HttpStatus.BAD_REQUEST.value(),
+				request.getRequestURI(),
+				erros);
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resposta);
+	}
+
+}
