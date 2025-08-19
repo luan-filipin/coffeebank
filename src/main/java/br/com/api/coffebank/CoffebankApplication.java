1dba91fcc4bd7ab32bb599929824b0c3b62474ea
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-08-19 13:25:04 -0300

    Primeiro commit.
    - Commit do projeto.

---
+++ src/main/java/br/com/api/coffebank/CoffebankApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.coffebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffebankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffebankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/coffebank/CoffebankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.coffebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffebankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
