4ec689b015e9bd751aa41aaaf7606cb58e98e2a1
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-08-20 11:03:41 -0300

    Update ClienteController
    - Renomeado o nome do Dto de resposta.
    - Implementado um novo Dto para pesquisa dos clientes pelo codigoCliente.

--- src/main/java/br/com/api/coffebank/controller/ClienteController.java
+++ src/main/java/br/com/api/coffebank/controller/ClienteController.java
@@ -3,13 +3,15 @@ package br.com.api.coffebank.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.coffebank.dto.CriarClienteDto;
-import br.com.api.coffebank.dto.resposta.RespostaCriacaoClienteDto;
+import br.com.api.coffebank.dto.resposta.RespostaClienteDto;
 import br.com.api.coffebank.service.ClienteService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -23,8 +25,15 @@ public class ClienteController {
 	private final ClienteService clienteService;
 	
 	@PostMapping("/cliente")
-	public ResponseEntity<RespostaCriacaoClienteDto> criarCliente(@RequestBody @Valid CriarClienteDto dto){
-		RespostaCriacaoClienteDto clienteCriado = clienteService.criarCliente(dto);
+	public ResponseEntity<RespostaClienteDto> criarCliente(@RequestBody @Valid CriarClienteDto dto){
+		RespostaClienteDto clienteCriado = clienteService.criarCliente(dto);
 		return ResponseEntity.status(HttpStatus.CREATED).body(clienteCriado);
+	}
+	
+	
+	@GetMapping("/cliente")
+	public ResponseEntity<RespostaClienteDto> buscarClientePeloCodigo(@RequestParam Long codigo){
+		RespostaClienteDto cliente = clienteService.buscarClientePeloCodigo(codigo);
+		return ResponseEntity.ok(cliente);
 	}
 }
