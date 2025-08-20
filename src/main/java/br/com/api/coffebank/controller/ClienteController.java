f4a94792ee2c2f77f0ccd6a40a88f9a8fbf57192
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-08-20 14:04:16 -0300

    Update ClienteController
    - Implementado rota para deletar um cliente pelo codigo.
    - Removido o caminho direto da rota e deixado apenas o da classe.

--- src/main/java/br/com/api/coffebank/controller/ClienteController.java
+++ src/main/java/br/com/api/coffebank/controller/ClienteController.java
@@ -3,6 +3,7 @@ package br.com.api.coffebank.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.api.coffebank.dto.CriarClienteDto;
+import br.com.api.coffebank.dto.RequisicaoClienteDto;
 import br.com.api.coffebank.dto.resposta.RespostaClienteDto;
 import br.com.api.coffebank.service.ClienteService;
 import jakarta.validation.Valid;
@@ -19,21 +20,27 @@ import lombok.RequiredArgsConstructor;
 @Validated
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api")
+@RequestMapping("api/cliente")
 public class ClienteController {
 
 	private final ClienteService clienteService;
 	
-	@PostMapping("/cliente")
-	public ResponseEntity<RespostaClienteDto> criarCliente(@RequestBody @Valid CriarClienteDto dto){
+	@PostMapping
+	public ResponseEntity<RespostaClienteDto> criarCliente(@RequestBody @Valid RequisicaoClienteDto dto){
 		RespostaClienteDto clienteCriado = clienteService.criarCliente(dto);
 		return ResponseEntity.status(HttpStatus.CREATED).body(clienteCriado);
 	}
 	
 	
-	@GetMapping("/cliente")
+	@GetMapping
 	public ResponseEntity<RespostaClienteDto> buscarClientePeloCodigo(@RequestParam Long codigo){
 		RespostaClienteDto cliente = clienteService.buscarClientePeloCodigo(codigo);
 		return ResponseEntity.ok(cliente);
+	}
+	
+	@DeleteMapping
+	public ResponseEntity<Void> deletaClientePeloCodigo(@RequestParam Long codigo){
+		clienteService.deletaClientePeloCodigo(codigo);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 }
