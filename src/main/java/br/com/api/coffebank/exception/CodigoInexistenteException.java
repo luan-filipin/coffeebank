e8829b3df4cf2a7227f60810a25acf2152c2e93e
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-08-20 11:23:43 -0300

    Update CodigoInexistenteException
    - Renomeado o atributo para que fique claro o retorno da informação.

--- src/main/java/br/com/api/coffebank/exception/CodigoInexistenteException.java
+++ src/main/java/br/com/api/coffebank/exception/CodigoInexistenteException.java
@@ -2,10 +2,10 @@ package br.com.api.coffebank.exception;
 
 public class CodigoInexistenteException extends RuntimeException{
 	
-	private static final String CPF_INEXISTENTE = "O cliente não existe!";
+	private static final String CODIGO_INEXISTENTE = "O cliente não existe!";
 	
 	public CodigoInexistenteException() {
-		super(CPF_INEXISTENTE);
+		super(CODIGO_INEXISTENTE);
 	}
 
 }
