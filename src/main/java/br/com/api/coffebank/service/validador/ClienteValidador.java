1c9082771179e7756c864eaf909e388bf97cd513
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-08-20 11:09:23 -0300

    Update ClienteValidador.
    - Implementado um novo validador para verificar se o cliente existe no banco.

--- src/main/java/br/com/api/coffebank/service/validador/ClienteValidador.java
+++ src/main/java/br/com/api/coffebank/service/validador/ClienteValidador.java
@@ -2,6 +2,8 @@ package br.com.api.coffebank.service.validador;
 
 import org.springframework.stereotype.Component;
 
+import br.com.api.coffebank.entity.Cliente;
+import br.com.api.coffebank.exception.CodigoInexistenteException;
 import br.com.api.coffebank.exception.CpfJaExisteException;
 import br.com.api.coffebank.repository.ClienteRepository;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +18,10 @@ public class ClienteValidador {
 		if(clienteRepository.findByDadosPessoais_Cpf(cpf).isPresent()) {
 			throw new CpfJaExisteException();
 		}
+	}
+	
+	public Cliente validaSeOCodigoDoClienteExiste(Long codigo) {
+		return clienteRepository.findByCodigoCliente(codigo).orElseThrow(CodigoInexistenteException::new);
 	}
 	
 }
