4ba2c0df919c3f089570a495c4f8adf7b40f1642
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-08-20 14:04:37 -0300

    Update ClienteMapper
    - Renomeado o Dto.

--- src/main/java/br/com/api/coffebank/mapper/ClienteMapper.java
+++ src/main/java/br/com/api/coffebank/mapper/ClienteMapper.java
@@ -2,7 +2,7 @@ package br.com.api.coffebank.mapper;
 
 import org.mapstruct.Mapper;
 
-import br.com.api.coffebank.dto.CriarClienteDto;
+import br.com.api.coffebank.dto.RequisicaoClienteDto;
 import br.com.api.coffebank.dto.resposta.RespostaClienteDto;
 import br.com.api.coffebank.entity.Cliente;
 
@@ -11,5 +11,5 @@ public interface ClienteMapper {
 
 	RespostaClienteDto toDto(Cliente entity);
 	
-	Cliente toEntity(CriarClienteDto dto);
+	Cliente toEntity(RequisicaoClienteDto dto);
 }
