73b348f011042301ecbd20e89fbfcfad86234145
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-08-28 13:25:55 -0300

    Create ContaService, ContaServiceImp
    - Implementado o metodo para criar a conta do cliente.
    - O numero da conta é gerado de forma automática com o numero do ID do cliente.

---
+++ src/main/java/br/com/api/coffebank/service/ContaService.java
@@ -0,0 +1,6 @@
+package br.com.api.coffebank.service;
+
+public interface ContaService {
+
+	void criarConta(Long codigoCliente);
+}
---
+++ src/main/java/br/com/api/coffebank/service/ContaServiceImp.java
@@ -0,0 +1,33 @@
+package br.com.api.coffebank.service;
+
+import java.math.BigDecimal;
+
+import org.springframework.stereotype.Service;
+
+import br.com.api.coffebank.entity.Conta;
+import br.com.api.coffebank.entity.TipoConta;
+import br.com.api.coffebank.repository.ContaRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class ContaServiceImp implements ContaService{
+
+	private final ContaRepository contaRepository;
+
+	@Transactional
+	@Override
+	public void criarConta(Long codigoCliente) {
+		Conta conta = new Conta();
+		conta.setCodigoCliente(codigoCliente);
+		conta.setNumeroConta(gerarNumeroConta(codigoCliente));
+		conta.setSaldo(BigDecimal.ZERO);
+		conta.setTipoConta(TipoConta.CORRENTE);
+		contaRepository.save(conta);
+	}
+	
+	private String gerarNumeroConta(Long codigoCliente) {
+	    return String.format("%06d", codigoCliente); // ou qualquer regra que quiser
+	}
+}
