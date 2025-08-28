6739f348e701b6de5e58428bb7deeb428f241f71
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-08-28 13:28:18 -0300

    Update ClienteMapper
    - Implementado o metodo para mapear as informações de Dto para entity e vice-versa e ignorar o CPF

--- src/main/java/br/com/api/coffebank/mapper/ClienteMapper.java
+++ src/main/java/br/com/api/coffebank/mapper/ClienteMapper.java
@@ -1,6 +1,8 @@
 package br.com.api.coffebank.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 import br.com.api.coffebank.dto.RequisicaoClienteDto;
 import br.com.api.coffebank.dto.resposta.RespostaClienteDto;
@@ -12,4 +14,7 @@ public interface ClienteMapper {
 	RespostaClienteDto toDto(Cliente entity);
 	
 	Cliente toEntity(RequisicaoClienteDto dto);
+	
+	@Mapping(target = "dadosPessoais.cpf", ignore = true)
+	void atualizaDto(RequisicaoClienteDto dto, @MappingTarget Cliente entity);
 }
