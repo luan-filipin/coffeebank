b42498c53eedb1084f6c12c45fd9b18d29979e37
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-08-28 13:16:15 -0300

    Update ClienteControllerTest
    - Refatorado codigo existente.
    - Implementado o teste de sucesso e falha para o controller de Update.

--- src/test/java/br/com/api/coffebank/controller/ClienteControllerTest.java
+++ src/test/java/br/com/api/coffebank/controller/ClienteControllerTest.java
@@ -1,7 +1,6 @@
 package br.com.api.coffebank.controller;
 
 import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
@@ -33,8 +32,6 @@ import br.com.api.coffebank.dto.resposta.RespostaClienteDto;
 import br.com.api.coffebank.dto.resposta.RespostaClienteEnderecoDto;
 import br.com.api.coffebank.entity.TipoSexo;
 import br.com.api.coffebank.service.ClienteService;
-import static org.hamcrest.Matchers.containsString;
-
 
 @WebMvcTest(ClienteController.class)
 @AutoConfigureMockMvc(addFilters = false)
@@ -43,121 +40,184 @@ class ClienteControllerTest {
 
 	@Autowired
 	private MockMvc mockMvc;
-	
+
 	@Autowired
 	private ObjectMapper objectMapper;
-	
+
 	@MockitoBean
 	private ClienteService clienteService;
-	
+
 	@DisplayName("POST - Deve criar o cliente com sucesso.")
 	@Test
-	void deveCriarClienteComSucesso() throws Exception{
-		
-	    LocalDate dataNascimento = LocalDate.of(1992, 5, 2);
-		
-		RequisicaoClienteDadosPessoaisDto dtoDadosPessoaisEntrada = new RequisicaoClienteDadosPessoaisDto("Luan", "teste@teste.com", TipoSexo.MASCULINO, "12345678910", "33333333", dataNascimento, "Brasileiro");
-		RequisicaoClienteEnderecoDto dtoEnderecoEntrada = new RequisicaoClienteEnderecoDto("Rua teste", "83", "BairroTeste", "Sao Paulo", "Casa", "Brasil");
+	void deveCriarClienteComSucesso() throws Exception {
+
+		LocalDate dataNascimento = LocalDate.of(1992, 5, 2);
+
+		RequisicaoClienteDadosPessoaisDto dtoDadosPessoaisEntrada = new RequisicaoClienteDadosPessoaisDto("Luan",
+				"teste@teste.com", TipoSexo.MASCULINO, "12345678910", "33333333", dataNascimento, "Brasileiro");
+		RequisicaoClienteEnderecoDto dtoEnderecoEntrada = new RequisicaoClienteEnderecoDto("Rua teste", "83",
+				"BairroTeste", "Sao Paulo", "Casa", "Brasil");
 		RequisicaoClienteDto dtoEntrada = new RequisicaoClienteDto(dtoDadosPessoaisEntrada, dtoEnderecoEntrada);
-	
-		RespostaClienteDadosPessoaisDto dtoDadosPessoaisEsperado = new RespostaClienteDadosPessoaisDto("Luan", "teste@teste.com", TipoSexo.MASCULINO, "12345678910", "33333333", dataNascimento, "Brasileiro");
-		RespostaClienteEnderecoDto dtoEnderecoEsperado = new RespostaClienteEnderecoDto("Rua teste", "83", "BairroTeste", "Sao Paulo", "Casa", "Brasil");
+
+		RespostaClienteDadosPessoaisDto dtoDadosPessoaisEsperado = new RespostaClienteDadosPessoaisDto("Luan",
+				"teste@teste.com", TipoSexo.MASCULINO, "12345678910", "33333333", dataNascimento, "Brasileiro");
+		RespostaClienteEnderecoDto dtoEnderecoEsperado = new RespostaClienteEnderecoDto("Rua teste", "83",
+				"BairroTeste", "Sao Paulo", "Casa", "Brasil");
 		RespostaClienteDto dtoEsperado = new RespostaClienteDto(1L, dtoDadosPessoaisEsperado, dtoEnderecoEsperado);
-		
+
 		when(clienteService.criarCliente(dtoEntrada)).thenReturn(dtoEsperado);
-		
-		mockMvc.perform(post("/api/cliente")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(dtoEntrada)))
-		.andExpect(status().isCreated())
-		.andExpect(jsonPath("$.codigoCliente").value(1L))
-		.andExpect(jsonPath("$.dadosPessoais.nome").value("Luan"))
-		.andExpect(jsonPath("$.dadosPessoais.cpf").value("12345678910"));
-		
+
+		mockMvc.perform(post("/api/cliente").contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(dtoEntrada))).andExpect(status().isCreated())
+				.andExpect(jsonPath("$.codigoCliente").value(1L))
+				.andExpect(jsonPath("$.dadosPessoais.nome").value("Luan"))
+				.andExpect(jsonPath("$.dadosPessoais.cpf").value("12345678910"));
+
 	}
-	
+
 	@DisplayName("POST - Deve lançar exception se tiver algum campo nullo ou ausente.")
 	@Test
-	void deveLancarExceptionSeCampoForNulloOuAusente() throws Exception{
-		
+	void deveLancarExceptionSeCampoForNulloOuAusente() throws Exception {
+
 		LocalDate dataNascimento = LocalDate.of(1992, 5, 2);
-		
-		RequisicaoClienteDadosPessoaisDto dtoDadosPessoaisEntrada = new RequisicaoClienteDadosPessoaisDto("", "teste@teste.com", TipoSexo.MASCULINO, "12345678910", "33333333", dataNascimento, "Brasileiro");
-		RequisicaoClienteEnderecoDto dtoEnderecoEntrada = new RequisicaoClienteEnderecoDto("Rua teste", "83", "BairroTeste", "Sao Paulo", "Casa", "Brasil");
+
+		RequisicaoClienteDadosPessoaisDto dtoDadosPessoaisEntrada = new RequisicaoClienteDadosPessoaisDto("",
+				"teste@teste.com", TipoSexo.MASCULINO, "12345678910", "33333333", dataNascimento, "Brasileiro");
+		RequisicaoClienteEnderecoDto dtoEnderecoEntrada = new RequisicaoClienteEnderecoDto("Rua teste", "83",
+				"BairroTeste", "Sao Paulo", "Casa", "Brasil");
 		RequisicaoClienteDto dtoEntrada = new RequisicaoClienteDto(dtoDadosPessoaisEntrada, dtoEnderecoEntrada);
-		
-		mockMvc.perform(post("/api/cliente")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(dtoEntrada)))
-		.andExpect(status().isBadRequest())
-		.andExpect(jsonPath("$.status").value(400))
-		.andExpect(jsonPath("$.mensagem").value("Campos inválidos"))
-		.andExpect(jsonPath("$.path").value("/api/cliente"))
-		.andExpect(jsonPath("$.timestamp").exists())
-		.andExpect(jsonPath("$.erros[0].campo").value("dadosPessoais.nome"))
-		.andExpect(jsonPath("$.erros[0].mensagem").value("O campo 'nome' é obrigatorio!"));
+
+		mockMvc.perform(post("/api/cliente").contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(dtoEntrada))).andExpect(status().isBadRequest())
+				.andExpect(jsonPath("$.status").value(400)).andExpect(jsonPath("$.mensagem").value("Campos inválidos"))
+				.andExpect(jsonPath("$.path").value("/api/cliente")).andExpect(jsonPath("$.timestamp").exists())
+				.andExpect(jsonPath("$.erros[0].campo").value("dadosPessoais.nome"))
+				.andExpect(jsonPath("$.erros[0].mensagem").value("O campo 'nome' é obrigatorio!"));
 	}
-	
+
 	@DisplayName("GET - Deve buscar o cliente pelo codigo do cliente com sucesso.")
 	@Test
-	void deveBuscarClientePeloCodigoComSucesso() throws Exception{
-		
+	void deveBuscarClientePeloCodigoComSucesso() throws Exception {
+
 		Long codigoCliente = 1L;
-	    LocalDate dataNascimento = LocalDate.of(1992, 5, 2);
-		
-		RespostaClienteDadosPessoaisDto dtoDadosPessoaisEsperado = new RespostaClienteDadosPessoaisDto("Luan", "teste@teste.com", TipoSexo.MASCULINO, "12345678910", "33333333", dataNascimento, "Brasileiro");
-		RespostaClienteEnderecoDto dtoEnderecoEsperado = new RespostaClienteEnderecoDto("Rua teste", "83", "BairroTeste", "Sao Paulo", "Casa", "Brasil");
+		LocalDate dataNascimento = LocalDate.of(1992, 5, 2);
+
+		RespostaClienteDadosPessoaisDto dtoDadosPessoaisEsperado = new RespostaClienteDadosPessoaisDto("Luan",
+				"teste@teste.com", TipoSexo.MASCULINO, "12345678910", "33333333", dataNascimento, "Brasileiro");
+		RespostaClienteEnderecoDto dtoEnderecoEsperado = new RespostaClienteEnderecoDto("Rua teste", "83",
+				"BairroTeste", "Sao Paulo", "Casa", "Brasil");
 		RespostaClienteDto dtoEsperado = new RespostaClienteDto(1L, dtoDadosPessoaisEsperado, dtoEnderecoEsperado);
-		
+
 		when(clienteService.buscarClientePeloCodigo(codigoCliente)).thenReturn(dtoEsperado);
-		
-		mockMvc.perform(get("/api/cliente")
-				.param("codigo", String.valueOf(codigoCliente)))
-		.andExpect(status().isOk())
-        .andExpect(jsonPath("$.codigoCliente").value(1L))
-        .andExpect(jsonPath("$.dadosPessoais.nome").value("Luan"))
-        .andExpect(jsonPath("$.dadosPessoais.email").value("teste@teste.com"))
-        .andExpect(jsonPath("$.dadosPessoais.sexo").value("MASCULINO"))
-        .andExpect(jsonPath("$.dadosPessoais.cpf").value("12345678910"));
+
+		mockMvc.perform(get("/api/cliente/{codigo}", codigoCliente)).andExpect(status().isOk())
+				.andExpect(jsonPath("$.codigoCliente").value(1L))
+				.andExpect(jsonPath("$.dadosPessoais.nome").value("Luan"))
+				.andExpect(jsonPath("$.dadosPessoais.email").value("teste@teste.com"))
+				.andExpect(jsonPath("$.dadosPessoais.sexo").value("MASCULINO"))
+				.andExpect(jsonPath("$.dadosPessoais.cpf").value("12345678910"));
 	}
-	
+
 	@DisplayName("GET - Deve lançar exception se o campo estiver ausente.")
 	@Test
-	void deveLancarExceptionSeCampoEstiveAusente() throws Exception{
+	void deveLancarExceptionSeCampoEstiveAusente() throws Exception {
 
-	    mockMvc.perform(get("/api/cliente"))
-        .andExpect(status().isBadRequest())
-        .andExpect(jsonPath("$.status").value(400))
-        .andExpect(jsonPath("$.mensagem").value("O parâmetro 'codigo' é obrigatório!"))
-        .andExpect(jsonPath("$.path").value("/api/cliente"))
-        .andExpect(jsonPath("$.timestamp").exists());
-	
+		mockMvc.perform(get("/api/cliente")).andExpect(status().isMethodNotAllowed())
+				.andExpect(jsonPath("$.status").value(405))
+				.andExpect(jsonPath("$.mensagem").value("Método GET não é suportado para esta URL."))
+				.andExpect(jsonPath("$.path").value("/api/cliente")).andExpect(jsonPath("$.timestamp").exists());
+
 	}
-	
+
 	@DisplayName("DELETE - Deve deletar cliente com sucesso pelo codigo.")
 	@Test
-	void deveDeletarClientePeloCodigoComSucesso()throws Exception{
-		
+	void deveDeletarClientePeloCodigoComSucesso() throws Exception {
+
 		Long codigoCliente = 1L;
-		
+
 		doNothing().when(clienteService).deletaClientePeloCodigo(codigoCliente);
-		
-		mockMvc.perform(delete("/api/cliente")
-				.param("codigo", String.valueOf(codigoCliente)))
-		.andExpect(status().isNoContent());
+
+		mockMvc.perform(delete("/api/cliente/{codigo}", codigoCliente)).andExpect(status().isNoContent());
 	}
-	
+
 	@DisplayName("DELETE - Deve lançar exception caso nao tenha o parametro codigo.")
 	@Test
-	void deveLancarExceptionSeCodigoForAusente() throws Exception{
-		
-		mockMvc.perform(delete("/api/cliente"))
-		.andExpect(status().isBadRequest())
-        .andExpect(jsonPath("$.status").value(400))
-        .andExpect(jsonPath("$.mensagem").value("O parâmetro 'codigo' é obrigatório!"))
-        .andExpect(jsonPath("$.path").value("/api/cliente"))
-        .andExpect(jsonPath("$.timestamp").exists());	
-		
+	void deveLancarExceptionSeCodigoForAusente() throws Exception {
+
+		mockMvc.perform(delete("/api/cliente")).andExpect(status().isMethodNotAllowed())
+				.andExpect(jsonPath("$.status").value(405))
+				.andExpect(jsonPath("$.mensagem").value("Método DELETE não é suportado para esta URL."))
+				.andExpect(jsonPath("$.path").value("/api/cliente")).andExpect(jsonPath("$.timestamp").exists());
+
 	}
-	
+
+	@DisplayName("UPDATE - Deve atualizar um cliente com sucesso.")
+	@Test
+	void deveAtualizarClienteComSucesso() throws Exception {
+
+		Long codigoCliente = 1L;
+		LocalDate dataNascimento = LocalDate.of(1992, 5, 2);
+
+		RequisicaoClienteDadosPessoaisDto dtoDadosPessoaisEntrada = new RequisicaoClienteDadosPessoaisDto("Luan",
+				"teste@teste.com", TipoSexo.MASCULINO, "12345678910", "33333333", dataNascimento, "Brasileiro");
+		RequisicaoClienteEnderecoDto dtoEnderecoEntrada = new RequisicaoClienteEnderecoDto("Rua teste", "83",
+				"BairroTeste", "Sao Paulo", "Casa", "Brasil");
+		RequisicaoClienteDto dtoEntrada = new RequisicaoClienteDto(dtoDadosPessoaisEntrada, dtoEnderecoEntrada);
+
+		RespostaClienteDadosPessoaisDto dtoDadosPessoaisEsperado = new RespostaClienteDadosPessoaisDto("Luan",
+				"teste@teste.com", TipoSexo.MASCULINO, "12345678910", "33333333", dataNascimento, "Brasileiro");
+		RespostaClienteEnderecoDto dtoEnderecoEsperado = new RespostaClienteEnderecoDto("Rua teste", "83",
+				"BairroTeste", "Sao Paulo", "Casa", "Brasil");
+		RespostaClienteDto dtoEsperado = new RespostaClienteDto(1L, dtoDadosPessoaisEsperado, dtoEnderecoEsperado);
+
+		when(clienteService.atualizaClientePeloCodigo(dtoEntrada, codigoCliente)).thenReturn(dtoEsperado);
+
+		mockMvc.perform(put("/api/cliente/{codigo}", codigoCliente).contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(dtoEntrada))).andExpect(status().isOk())
+				.andExpect(jsonPath("$.codigoCliente").value(1L))
+				.andExpect(jsonPath("$.dadosPessoais.nome").value("Luan"))
+				.andExpect(jsonPath("$.dadosPessoais.cpf").value("12345678910"));
+	}
+
+	@DisplayName("UPDATE - Deve lançar exceptions e o campo do corpo estiver ausente.")
+	@Test
+	void deveLancarExceptionSeCampoDoCorpoEstiverAusente() throws Exception {
+
+		Long codigoCliente = 1L;
+		LocalDate dataNascimento = LocalDate.of(1992, 5, 2);
+
+		RequisicaoClienteDadosPessoaisDto dtoDadosPessoaisEntrada = new RequisicaoClienteDadosPessoaisDto("",
+				"teste@teste.com", TipoSexo.MASCULINO, "12345678910", "33333333", dataNascimento, "Brasileiro");
+		RequisicaoClienteEnderecoDto dtoEnderecoEntrada = new RequisicaoClienteEnderecoDto("Rua teste", "83",
+				"BairroTeste", "Sao Paulo", "Casa", "Brasil");
+		RequisicaoClienteDto dtoEntrada = new RequisicaoClienteDto(dtoDadosPessoaisEntrada, dtoEnderecoEntrada);
+
+		mockMvc.perform(put("/api/cliente/{codigo}", codigoCliente).contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(dtoEntrada))).andExpect(status().isBadRequest())
+				.andExpect(jsonPath("$.status").value(400)).andExpect(jsonPath("$.mensagem").value("Campos inválidos"))
+				.andExpect(jsonPath("$.path").value("/api/cliente/1")).andExpect(jsonPath("$.timestamp").exists())
+				.andExpect(jsonPath("$.erros[0].campo").value("dadosPessoais.nome"))
+				.andExpect(jsonPath("$.erros[0].mensagem").value("O campo 'nome' é obrigatorio!"));
+
+	}
+
+	@DisplayName("UPDATE - Deve lançar exceptions se o parametro do codigo nao for informado.")
+	@Test
+	void deveLancarExceptionSeParametroDoCodigoNaoForInformado() throws Exception {
+
+		LocalDate dataNascimento = LocalDate.of(1992, 5, 2);
+
+		RequisicaoClienteDadosPessoaisDto dtoDadosPessoaisEntrada = new RequisicaoClienteDadosPessoaisDto("Luan",
+				"teste@teste.com", TipoSexo.MASCULINO, "12345678910", "33333333", dataNascimento, "Brasileiro");
+		RequisicaoClienteEnderecoDto dtoEnderecoEntrada = new RequisicaoClienteEnderecoDto("Rua teste", "83",
+				"BairroTeste", "Sao Paulo", "Casa", "Brasil");
+		RequisicaoClienteDto dtoEntrada = new RequisicaoClienteDto(dtoDadosPessoaisEntrada, dtoEnderecoEntrada);
+
+		mockMvc.perform(put("/api/cliente").contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(dtoEntrada)))
+				.andExpect(status().isMethodNotAllowed())
+				.andExpect(jsonPath("$.status").value(405))
+				.andExpect(jsonPath("$.mensagem").value("Método PUT não é suportado para esta URL."))
+				.andExpect(jsonPath("$.path").value("/api/cliente")).andExpect(jsonPath("$.timestamp").exists());
+	}
 }
