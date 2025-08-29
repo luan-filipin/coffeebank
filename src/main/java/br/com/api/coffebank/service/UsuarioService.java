5051ef3df8f17eae62b1fa9e563f7c9f68ef303e
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-08-29 13:08:05 -0300

    Create UsuarioService.java
    - Implementado a logica para criarUsuario.

---
+++ src/main/java/br/com/api/coffebank/service/UsuarioService.java
@@ -0,0 +1,38 @@
+package br.com.api.coffebank.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import br.com.api.coffebank.dto.RequisicaoUsuarioDto;
+import br.com.api.coffebank.entity.Usuario;
+import br.com.api.coffebank.mapper.UsuarioMapper;
+import br.com.api.coffebank.repository.UsuarioRepository;
+import br.com.api.coffebank.service.validador.UsuarioValidador;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class UsuarioService implements UserDetailsService {
+
+	private final UsuarioRepository usuarioRepository;
+	private final UsuarioValidador usuarioValidador;
+	private final UsuarioMapper usuarioMapper;
+	private final PasswordEncoder passwordEncoder;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return usuarioRepository.findByUsuario(username);
+	}
+
+	@Transactional
+	public void criarUsuario(RequisicaoUsuarioDto usuarioDto) {
+		usuarioValidador.validaSeUsuarioExiste(usuarioDto.usuario());
+		Usuario usuario = usuarioMapper.toEntity(usuarioDto);
+		usuario.setSenha(passwordEncoder.encode(usuario.getPassword()));
+		usuarioRepository.save(usuario);
+	}
+}
