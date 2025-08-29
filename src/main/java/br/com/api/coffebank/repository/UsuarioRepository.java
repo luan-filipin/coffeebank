149727b0c1b989e26f79c2157bf504dfbf2a2389
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-08-29 13:17:03 -0300

    Create UsuarioRepository.java
    - implementado o metodo para buscar o usuario e outro para ver se o mesmo existe.

---
+++ src/main/java/br/com/api/coffebank/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package br.com.api.coffebank.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import br.com.api.coffebank.entity.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+	UserDetails findByUsuario(String usuario);
+	
+	boolean existsByUsuario(String usuario);
+}
