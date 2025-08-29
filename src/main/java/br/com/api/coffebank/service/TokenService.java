4f24c275773aadae605d8b11ed681794fc020d4b
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-08-29 13:09:32 -0300

    Create TokenService.java
    - Implementado método para gerarToken, validarToken e atribuir tempo de expiração.

---
+++ src/main/java/br/com/api/coffebank/service/TokenService.java
@@ -0,0 +1,55 @@
+package br.com.api.coffebank.service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+
+import br.com.api.coffebank.entity.Usuario;
+import br.com.api.coffebank.exception.ErroAoGerarTokenException;
+import br.com.api.coffebank.exception.TokenInvalidoOuExpiradoException;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class TokenService {
+
+	private String secretKey = "secret";
+	
+	public String gerarToken(Usuario usuario) {
+		try {
+			Algorithm algorithm = Algorithm.HMAC256(secretKey);
+			return JWT.create()
+					.withIssuer("auth-api")
+					.withSubject(usuario.getUsuario())
+					.withExpiresAt(genExpirationDate())
+					.sign(algorithm);
+			
+		} catch (JWTCreationException exception) {
+			throw new ErroAoGerarTokenException(exception);
+		}
+	}
+	
+	public String validarToken(String token) {
+		try {
+			Algorithm algorithm = Algorithm.HMAC256(secretKey);
+			return JWT.require(algorithm)
+					.withIssuer("auth-api")
+					.build()
+					.verify(token)
+					.getSubject();
+		} catch (JWTVerificationException exception) {
+			throw new TokenInvalidoOuExpiradoException(exception);
+		}
+	}
+	
+	private Instant genExpirationDate() {
+		return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+	}
+}
