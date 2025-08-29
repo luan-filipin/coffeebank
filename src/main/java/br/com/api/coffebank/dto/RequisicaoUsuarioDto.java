7cfb7b369c25c81c36ee2a13ce93657dc762b0c7
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-08-29 13:27:32 -0300

    Create RequisicaoUsuarioDto.java
    - Implementado Dto para realizar a criação do usuario e validar os campos solicitados.

---
+++ src/main/java/br/com/api/coffebank/dto/RequisicaoUsuarioDto.java
@@ -0,0 +1,12 @@
+package br.com.api.coffebank.dto;
+
+import br.com.api.coffebank.entity.enums.UserRole;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record RequisicaoUsuarioDto(
+		@NotBlank(message = "O campo usuario é obrigatorio!") String usuario,
+		@NotBlank(message = "O campo senha é obrigatorio!") String senha,
+		@NotNull(message = "O campo role é obrigatorio!") UserRole role) {
+
+}
