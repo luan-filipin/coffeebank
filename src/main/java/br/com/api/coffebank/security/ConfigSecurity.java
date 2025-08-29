f751dc513f055bfcad3cb266f87e5abaad05d834
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-08-29 13:15:21 -0300

    Update ConfigSecurity.java
    - Configurado para que todas as rotas precisem de acesso com token.
    - Implementado metodo para cryptografar.
    - Implementado metodo para autenticar.

--- src/main/java/br/com/api/coffebank/security/ConfigSecurity.java
+++ src/main/java/br/com/api/coffebank/security/ConfigSecurity.java
@@ -3,28 +3,45 @@ package br.com.api.coffebank.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class ConfigSecurity {
 
-	private static final String ENDPOINT_BASIC = "/api/cliente";
-	
+	private final SecurityFilter securityFilter;
+
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)throws Exception{
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 		return httpSecurity.csrf(csrf -> csrf.disable())
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests(authorize -> authorize
-						.requestMatchers(HttpMethod.POST, ENDPOINT_BASIC).permitAll()
-						.requestMatchers(HttpMethod.GET, ENDPOINT_BASIC + "/**").permitAll()
-						.requestMatchers(HttpMethod.DELETE,ENDPOINT_BASIC + "/**").permitAll()
-						.requestMatchers(HttpMethod.PUT,ENDPOINT_BASIC + "/**").permitAll()
-						.anyRequest().authenticated()
-						)
+						.requestMatchers(HttpMethod.POST, "/usuario/token").permitAll()
+						.requestMatchers(HttpMethod.POST, "/usuario/**").permitAll().anyRequest()
+						.authenticated())
+				.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
 				.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+			throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
 	}
 }
