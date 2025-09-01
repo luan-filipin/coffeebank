21f33ce2fc5e92726fb9a2be8b48d9acea940760
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-09-01 12:38:27 -0300

    Update ContaConsumerImp.java
    - Implementado a logica para ouvir quando o cliente for deletado.

--- src/main/java/br/com/api/coffebank/service/event/consumer/ContaConsumerImp.java
+++ src/main/java/br/com/api/coffebank/service/event/consumer/ContaConsumerImp.java
@@ -3,7 +3,7 @@ package br.com.api.coffebank.service.event.consumer;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-import br.com.api.coffebank.dto.event.ClienteCriadoEventDto;
+import br.com.api.coffebank.dto.event.ClienteEventDto;
 import br.com.api.coffebank.service.ContaService;
 import lombok.RequiredArgsConstructor;
 
@@ -15,8 +15,14 @@ public class ContaConsumerImp implements ContaConsumer{
 
 	@KafkaListener(topics = "cliente-criado", groupId = "coffebank-group")
 	@Override
-	public void ouvirClienteCriadoKafka(ClienteCriadoEventDto event) {
+	public void ouvirClienteCriadoKafka(ClienteEventDto event) {
 		contaService.criarConta(event.codigoCliente());
+	}
+
+	@KafkaListener(topics = "cliente-deletado", groupId = "coffebank-group")
+	@Override
+	public void ouvirClienteDeletadoKafka(ClienteEventDto event) {
+		contaService.deletarConta(event.codigoCliente());
 	}
 
 }
