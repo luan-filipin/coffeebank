3940e13dbe951d07320a78ab079e1eb745f21994
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-09-01 12:37:43 -0300

    Update ContaConsumer.java
    - Implementado a interface do metodo para ouvir quando o cliente for deletado.

--- src/main/java/br/com/api/coffebank/service/event/consumer/ContaConsumer.java
+++ src/main/java/br/com/api/coffebank/service/event/consumer/ContaConsumer.java
@@ -1,9 +1,10 @@
 package br.com.api.coffebank.service.event.consumer;
 
-import br.com.api.coffebank.dto.event.ClienteCriadoEventDto;
+import br.com.api.coffebank.dto.event.ClienteEventDto;
 
 public interface ContaConsumer {
 
-	void ouvirClienteCriadoKafka(ClienteCriadoEventDto event);
+	void ouvirClienteCriadoKafka(ClienteEventDto event);
+	void ouvirClienteDeletadoKafka(ClienteEventDto event);
 	
 }
