dc132bee85e761fa2ea5d1861027577476390dba
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-09-01 12:39:25 -0300

    Update ClienteProducerImp.java
    - Implementado a logica para o metodo que enviar o evento ao Kafka quando o cliente é criado.

--- src/main/java/br/com/api/coffebank/service/event/producer/ClienteProducerImp.java
+++ src/main/java/br/com/api/coffebank/service/event/producer/ClienteProducerImp.java
@@ -3,20 +3,26 @@ package br.com.api.coffebank.service.event.producer;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-import br.com.api.coffebank.dto.event.ClienteCriadoEventDto;
+import br.com.api.coffebank.dto.event.ClienteEventDto;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
 public class ClienteProducerImp implements ClienteProducer{
 	
-	private final KafkaTemplate<String, ClienteCriadoEventDto> kafkaTemplate;
+	private final KafkaTemplate<String, ClienteEventDto> kafkaTemplate;
 
 	@Override
 	public void enviarClienteCriadoKafka(Long codigoCliente) {
-		ClienteCriadoEventDto event = new ClienteCriadoEventDto(codigoCliente);
+		ClienteEventDto event = new ClienteEventDto(codigoCliente);
 		kafkaTemplate.send("cliente-criado", event);
 		
+	}
+
+	@Override
+	public void enviarClienteDeletadoKafka(Long codigoCliente) {
+		ClienteEventDto event = new ClienteEventDto(codigoCliente);
+		kafkaTemplate.send("cliente-deletado", event);
 	}
 
 }
