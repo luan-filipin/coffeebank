092bb761566b35c970bc37e2a752470a126d57d5
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-09-01 11:54:19 -0300

    Update KafkaTopicConfig.java
    - Alterado a configuração para que recebe as informações do tópico pelo application.properties.

--- src/main/java/br/com/api/coffebank/config/KafkaTopicConfig.java
+++ src/main/java/br/com/api/coffebank/config/KafkaTopicConfig.java
@@ -1,15 +1,24 @@
 package br.com.api.coffebank.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class KafkaTopicConfig {
 	
+	@Value("${kafka.topicos}")
+	private String[] topicos;
+	
     @Bean
-    public NewTopic clienteCriadoTopic() {
-        return new NewTopic("cliente-criado", 1, (short) 1);
+    public List<NewTopic> criarTopicos() {
+        return Arrays.stream(topicos)
+        		.map(nome -> new NewTopic(nome, 1, (short)1))
+        		.toList();
     }
 
 }
