532b7497fe026e8444029fc78a9626a92caf546c
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-09-02 11:26:38 -0300

    Update ContaValidador.java
    - Implementado validações: validaSeContaExisteERetornaEntidade, validaSeValorNullOuNegativo, validaSeSaldoSuficienteParaSacar

--- src/main/java/br/com/api/coffebank/service/validador/ContaValidador.java
+++ src/main/java/br/com/api/coffebank/service/validador/ContaValidador.java
@@ -1,20 +1,41 @@
 package br.com.api.coffebank.service.validador;
 
+import java.math.BigDecimal;
+
 import org.springframework.stereotype.Component;
 
+import br.com.api.coffebank.entity.Conta;
 import br.com.api.coffebank.exception.NumeroDaContaNaoExisteException;
+import br.com.api.coffebank.exception.SaldoInsuficienteParaSaqueException;
+import br.com.api.coffebank.exception.ValorNegativoException;
 import br.com.api.coffebank.repository.ContaRepository;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Component
 public class ContaValidador {
-	
+
 	private final ContaRepository contaRepository;
 
 	public void validaSeContaExiste(Long numeroConta) {
-		if(!contaRepository.existsByCodigoCliente(numeroConta)) {
+		if (!contaRepository.existsByCodigoCliente(numeroConta)) {
 			throw new NumeroDaContaNaoExisteException();
+		}
+	}
+
+	public Conta validaSeContaExisteERetornaEntidade(Long codigoCliente) {
+		return contaRepository.findByCodigoCliente(codigoCliente).orElseThrow(NumeroDaContaNaoExisteException::new);
+	}
+	
+	public void validaSeValorNullOuNegativo(BigDecimal valor) {
+		if(valor == null || valor.compareTo(BigDecimal.ZERO) <= 0){
+			throw new ValorNegativoException();
+		}
+	}
+	
+	public void validaSeSaldoSuficienteParaSacar(BigDecimal saldo, BigDecimal valorSaque) {
+		if(saldo.compareTo(valorSaque) < 0) {
+			throw new SaldoInsuficienteParaSaqueException();
 		}
 	}
 }
