6ac860fe6aed34b427d8ca7e188c0c20a77090e0
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-09-02 11:31:17 -0300

    Update ContaRepository.java
    - Implementado um metodo para verificar se a conta existe e se existir retornar o mesmo.

--- src/main/java/br/com/api/coffebank/repository/ContaRepository.java
+++ src/main/java/br/com/api/coffebank/repository/ContaRepository.java
@@ -1,5 +1,7 @@
 package br.com.api.coffebank.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.api.coffebank.entity.Conta;
@@ -7,6 +9,8 @@ import br.com.api.coffebank.entity.Conta;
 public interface ContaRepository extends JpaRepository<Conta, Long>{
 
 	boolean existsByCodigoCliente(Long codigoCliente);
+	
+	Optional<Conta> findByCodigoCliente(Long codigoCliente);
 	
 	void deleteByCodigoCliente(Long codigoCliente);
 }
