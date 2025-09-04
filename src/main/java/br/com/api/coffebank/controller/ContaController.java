b04f5717f9ec9bdf39071f72b979be4c434eeddd
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-09-04 18:57:55 -0300

    Update ContaController.java
    - Implementado anotações para o Swagger

--- src/main/java/br/com/api/coffebank/controller/ContaController.java
+++ src/main/java/br/com/api/coffebank/controller/ContaController.java
@@ -8,22 +8,40 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.coffebank.dto.RequisicaoTransacaoValorDto;
 import br.com.api.coffebank.service.event.producer.ContaProducer;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/transacoes")
+@Tag(name = "Conta", description = "Endpoints para depositar e sacar valores")
 public class ContaController {
 	
 	private final ContaProducer contaProducer;
 
+    @Operation(
+            summary = "Deposita saldo na conta",
+            description = "Deposita valores na conta via evento kafka de forma automatica",
+            responses = {
+                    	@ApiResponse(responseCode = "202", description = "Deposito realizado com sucesso"),
+            }
+    )
 	@PostMapping("/depositar")
 	public ResponseEntity<Void> depositarValor(@RequestBody @Valid RequisicaoTransacaoValorDto dto) {
 		contaProducer.enviarValorDepositoKafka(dto.codigoCliente(), dto.valor());
 		return ResponseEntity.accepted().build();
 	}
-	
+    
+    @Operation(
+            summary = "Saca saldo da conta",
+            description = "Saca valores na conta via evento kafka de forma automatica",
+            responses = {
+                    	@ApiResponse(responseCode = "202", description = "Saque realizado com sucesso"),
+            }
+    )	
 	@PostMapping("/sacar")
 	public ResponseEntity<Void> sacarValor(@RequestBody @Valid RequisicaoTransacaoValorDto dto){
 		contaProducer.enviarValorSacarKafka(dto.codigoCliente(), dto.valor());
