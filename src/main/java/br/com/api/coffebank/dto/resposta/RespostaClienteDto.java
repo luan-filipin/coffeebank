46dd82b236f696d676171149a670896a82980c7e
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-09-04 18:55:49 -0300

    Update RespostaClienteDto.java
    - Implementado anotações para o Swagger

--- src/main/java/br/com/api/coffebank/dto/resposta/RespostaClienteDto.java
+++ src/main/java/br/com/api/coffebank/dto/resposta/RespostaClienteDto.java
@@ -1,9 +1,21 @@
 package br.com.api.coffebank.dto.resposta;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "DTO de resposta do cliente contendo dados pessoais e endereço")
 public record RespostaClienteDto(
 		
-		Long codigoCliente,
-		RespostaClienteDadosPessoaisDto dadosPessoais,
-		RespostaClienteEnderecoDto endereco) {
+	    @Schema(description = "Código único do cliente", example = "123")
+	    Long codigoCliente,
+	    
+	    @Schema(description = "Dados pessoais do cliente")
+	    RespostaClienteDadosPessoaisDto dadosPessoais,
+	    
+	    @Schema(description = "Endereço do cliente")
+	    RespostaClienteEnderecoDto endereco) {
 
 }
+
+
+
+
