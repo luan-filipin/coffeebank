1dfdabf8c16795b083232d546ddfca01512a6176
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-09-04 18:54:49 -0300

    Update RespostaUsuarioCriadoDto.java
    - Implementado anotações para o Swagger

--- src/main/java/br/com/api/coffebank/dto/resposta/RespotaUsuarioCriadoDto.java
+++ src/main/java/br/com/api/coffebank/dto/resposta/RespotaUsuarioCriadoDto.java
@@ -1,7 +1,15 @@
 package br.com.api.coffebank.dto.resposta;
 
 import br.com.api.coffebank.entity.enums.UserRole;
+import io.swagger.v3.oas.annotations.media.Schema;
 
-public record RespotaUsuarioCriadoDto (String username, UserRole role){
+@Schema(description = "Objeto de resposta retornado após a criação de um usuário")
+public record RespotaUsuarioCriadoDto(
 
-}
+    @Schema(description = "Nome de usuário criado", example = "Silva")
+    String username,
+    
+    @Schema(description = "Papel do usuário no sistema", example = "ADMIN")
+    UserRole role
+
+) {}
