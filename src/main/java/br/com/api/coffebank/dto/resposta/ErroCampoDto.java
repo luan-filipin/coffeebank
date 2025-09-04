c9293044bd524cc7789907fc4c4e1a6092520cbf
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-09-04 18:56:39 -0300

    Update ErroCampoSto.java
    - Implementado anotações para o Swagger

--- src/main/java/br/com/api/coffebank/dto/resposta/ErroCampoDto.java
+++ src/main/java/br/com/api/coffebank/dto/resposta/ErroCampoDto.java
@@ -1,7 +1,14 @@
 package br.com.api.coffebank.dto.resposta;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Detalhe de erro de validação de um campo específico")
 public record ErroCampoDto(
+		
+		@Schema(description = "Nome do campo com erro", example = "cpf")
 		String campo,
+		
+        @Schema(description = "Mensagem de validação associada ao campo", example = "CPF inválido")
 		String mensagem) {
 
 }
