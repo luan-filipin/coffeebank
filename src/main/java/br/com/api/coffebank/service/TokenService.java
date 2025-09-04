341493e925739bd561904c0239643c0a0e166c0e
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-09-04 18:34:02 -0300

    Update TokenService.java
    - Removido importe que não esta sendo utilizado.

--- src/main/java/br/com/api/coffebank/service/TokenService.java
+++ src/main/java/br/com/api/coffebank/service/TokenService.java
@@ -4,7 +4,6 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.auth0.jwt.JWT;
