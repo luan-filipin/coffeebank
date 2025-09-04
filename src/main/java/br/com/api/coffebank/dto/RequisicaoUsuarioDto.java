6678c72d32d8824cf607f0efec5e7dbf82a1d802
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-09-04 18:56:52 -0300

    Update RequisicaoUsuarioDto.java
    - Implementado anotações para o Swagger

--- src/main/java/br/com/api/coffebank/dto/RequisicaoUsuarioDto.java
+++ src/main/java/br/com/api/coffebank/dto/RequisicaoUsuarioDto.java
@@ -1,12 +1,17 @@
 package br.com.api.coffebank.dto;
 
 import br.com.api.coffebank.entity.enums.UserRole;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+@Schema(description = "Objeto para criação do usuario")
 public record RequisicaoUsuarioDto(
-		@NotBlank(message = "O campo usuario é obrigatorio!") String username,
-		@NotBlank(message = "O campo senha é obrigatorio!") String senha,
-		@NotNull(message = "O campo role é obrigatorio!") UserRole role) {
+
+		@Schema(description = "Username", example = "Silva") @NotBlank(message = "O campo usuario é obrigatorio!") String username,
+
+		@Schema(description = "Senha", example = "123456") @NotBlank(message = "O campo senha é obrigatorio!") String senha,
+
+		@Schema(description = "Role para acesso", example = "ADMIN, USER") @NotNull(message = "O campo role é obrigatorio!") UserRole role) {
 
 }
