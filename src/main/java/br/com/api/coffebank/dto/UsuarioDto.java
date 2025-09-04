f4c49b0f58dd9dcdc5aa83b0756af4719350e705
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-09-04 18:54:11 -0300

    Update UsuarioDto.java
    - Implementado anotações para o Swagger

--- src/main/java/br/com/api/coffebank/dto/UsuarioDto.java
+++ src/main/java/br/com/api/coffebank/dto/UsuarioDto.java
@@ -1,10 +1,16 @@
 package br.com.api.coffebank.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
+@Schema(description = "Objeto para solicitar o token")
 public record UsuarioDto(
-		
+		@Schema(description = "Username", 
+        example = "Silva")
 		@NotBlank(message = "O campo usuario é obrigatorio!") String username,
+		
+		@Schema(description = "Senha", 
+        example = "123456")
 		@NotBlank(message = "O campo senha é obrigatorio!") String senha) {
 
 }
