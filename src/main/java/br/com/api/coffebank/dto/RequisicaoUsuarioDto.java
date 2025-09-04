3fa6b14d661b3156791dc56b1083dfcc97361cf7
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-09-04 19:09:14 -0300

    Update RequisicaoUsuarioDto.java
    - Implementado anotação que limita o uso de letra e numeros apenas no campo senha.

--- src/main/java/br/com/api/coffebank/dto/RequisicaoUsuarioDto.java
+++ src/main/java/br/com/api/coffebank/dto/RequisicaoUsuarioDto.java
@@ -4,14 +4,23 @@ import br.com.api.coffebank.entity.enums.UserRole;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 @Schema(description = "Objeto para criação do usuario")
 public record RequisicaoUsuarioDto(
 
-		@Schema(description = "Username", example = "Silva") @NotBlank(message = "O campo usuario é obrigatorio!") String username,
+		@Schema(description = "Username", example = "Silva")
+		@NotBlank(message = "O campo usuario é obrigatorio!") 
+		String username,
 
-		@Schema(description = "Senha", example = "123456") @NotBlank(message = "O campo senha é obrigatorio!") String senha,
+		@Schema(description = "Senha", example = "123456") 
+		@Pattern(regexp = "^[a-zA-Z0-9]+$", message = "A senha deve conter apenas letras e números")
+		@NotBlank(message = "O campo senha é obrigatorio!") 
+		String senha,
 
-		@Schema(description = "Role para acesso", example = "ADMIN, USER") @NotNull(message = "O campo role é obrigatorio!") UserRole role) {
+		@Schema(description = "Role para acesso", example = "ADMIN, USER") 
+		@NotNull(message = "O campo role é obrigatorio!") 
+		UserRole role
+		) {
 
 }
