ebccb5e483d727a9a1c2476301d1400038fa76a9
Luan Brito Filipin <81528691+luan-filipin@users.noreply.github.com>
2025-09-04 18:57:38 -0300

    Update UsuarioController.java
    - Implementado anotações para o Swagger

--- src/main/java/br/com/api/coffebank/controller/UsuarioController.java
+++ src/main/java/br/com/api/coffebank/controller/UsuarioController.java
@@ -17,7 +17,9 @@ import br.com.api.coffebank.dto.resposta.RespotaUsuarioCriadoDto;
 import br.com.api.coffebank.entity.Usuario;
 import br.com.api.coffebank.service.TokenService;
 import br.com.api.coffebank.service.UsuarioService;
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -25,12 +27,20 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/usuario")
+@Tag(name = "Usuario", description = "Endpoints para criar usuario e solicitar token")
 public class UsuarioController {
 	
 	private final AuthenticationManager authenticationManager;
 	private final TokenService tokenService;
 	private final UsuarioService usuarioService; 
 	
+    @Operation(
+            summary = "Solicitar token",
+            description = "Retorna token para requisição após autenticação",
+            responses = {
+                    	@ApiResponse(responseCode = "200", description = "Token gerado com sucesso"),
+            }
+    )
 	@PostMapping("/token")
 	public ResponseEntity<RespostaTokenDto> token(@RequestBody @Valid UsuarioDto dto) {
 		var usernamePassword = new UsernamePasswordAuthenticationToken(dto.username(), dto.senha());
@@ -40,11 +50,16 @@ public class UsuarioController {
 		return ResponseEntity.ok(new RespostaTokenDto(token));
 	}
 	
+    @Operation(
+            summary = "Criar usuario",
+            description = "Cria um usuario para autenticação e solicitação do token",
+            responses = {
+                    	@ApiResponse(responseCode = "201", description = "Usuario criado com sucesso"),
+            }
+    )
 	@PostMapping
 	public ResponseEntity<RespotaUsuarioCriadoDto> criandoUsuario(@RequestBody @Valid RequisicaoUsuarioDto dto) {
 		RespotaUsuarioCriadoDto usuarioCriado = usuarioService.criarUsuario(dto);
 		return ResponseEntity.status(HttpStatus.CREATED).body(usuarioCriado);
-	}
-
-	
+	}	
 }
